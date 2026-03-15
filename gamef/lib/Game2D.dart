816109c42be1e2232d2e94db375b9d12c63b5332
Underlej <Bluesky.TFP@yandex.ru>
2026-03-15 14:54:00 +0500

    0.0.0.2
    создание тестовой карты

--- gamef/lib/Game2D.dart
+++ gamef/lib/Game2D.dart
@@ -1,5 +1,21 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:gamef/levels/level.dart';
 
 class Game2d extends FlameGame{
-  
+
+    late final CameraComponent cam;
+    final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    return super.onLoad();
+  }
 }
